c0651189b4b64aafc044bd95bf55da804d7694e2
AmuDaDev <amunkombyani@gmail.com>
2023-11-07 19:10:29 +0200

    Added missing bean

--- inventory-service/src/main/java/com/amuk3lani/shop/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/amuk3lani/shop/inventory/service/InventoryService.java
@@ -1,18 +1,5 @@
 package com.amuk3lani.shop.inventory.service;
 
-import com.amuk3lani.shop.inventory.repository.InventoryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@RequiredArgsConstructor
-public class InventoryService {
-
-    private final InventoryRepository inventoryRepository;
-
-    @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
-    }
+public interface InventoryService {
+    boolean isInStock(String skuCode);
 }
---
+++ inventory-service/src/main/java/com/amuk3lani/shop/inventory/service/InventoryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.amuk3lani.shop.inventory.service;
+
+import com.amuk3lani.shop.inventory.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryServiceImpl implements InventoryService{
+
+    private final InventoryRepository inventoryRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
